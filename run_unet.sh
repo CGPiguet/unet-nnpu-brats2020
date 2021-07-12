81fa9f79d39d0c05b527386338eb03498bb51dc9
Piguet Christian Gabriel (cp14h011) <cp14h011@submit01.ubelix.unibe.ch>
2021-07-12 16:04:19 +0200

    update run_unet.sh

--- run_unet.sh
+++ run_unet.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=end,fail
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=12G
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4 # 4 is the limit 
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:rtx2080ti:1
 #SBATCH --error="unet_error_numworker8.txt"
