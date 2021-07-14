c724ae4b154709f26ef1eaf6e10681d9c4eaa11d
Piguet Christian Gabriel (cp14h011) <cp14h011@submit01.ubelix.unibe.ch>
2021-07-14 15:45:08 +0200

    Update

--- run_unet.sh
+++ run_unet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="Unet BCELoss numWorker=8"
+#SBATCH --job-name="Unet BCELoss numWorker=4"
 #SBATCH --mail-user=christian.piguet@students.unibe.ch
 #SBATCH --mail-type=end,fail
 #SBATCH --time=24:00:00
@@ -7,8 +7,8 @@
 #SBATCH --cpus-per-task=4 # 4 is the limit 
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:rtx2080ti:1
-#SBATCH --error="unet_error_numworker8.txt"
-#SBATCH --output="unet_output_numworker8.txt"
+#SBATCH --error="unet_error.txt"
+#SBATCH --output="unet_output.txt"
 
 #### Your shell commands below this line ####
 module load Workspace/home Anaconda3 CUDA
@@ -16,4 +16,4 @@ eval "$(conda shell.bash hook)"
 conda activate unet
 
 
-python run_train.py -e 100 -p "BCELoss" -v False -b 16 -n "BCELoss_NumWorker8"
+python run_train.py -e 100 -p "BCELoss" -v False -b 16 -n "BCELoss_NumWorker4withoutValid" -nw 4
