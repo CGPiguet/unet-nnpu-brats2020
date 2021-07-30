c62006071328906b25da8edfd06eb21835c49c17
Piguet Christian Gabriel (cp14h011) <cp14h011@submit01.ubelix.unibe.ch>
2021-07-30 10:37:25 +0200

    Modified to work with 2D and 3D dataset

--- run_train.py
+++ run_train.py
@@ -18,7 +18,8 @@ from contextlib import redirect_stdout
 from preprocess3D import preprocess_brats2020_3D
 from preprocess2D import preprocess_brats2020_2D
 
-from dataset import PN_BraTS2020_Dataset, PU_BraTS2020_Dataset, BCE_BraTS2020_Dataset
+from dataset3D import BCE_BraTS2020_Dataset_3D, PN_BraTS2020_Dataset_3D, PU_BraTS2020_Dataset_3D
+from dataset2D import BCE_BraTS2020_Dataset_2D, PN_BraTS2020_Dataset_2D, PU_BraTS2020_Dataset_2D
 from model import unet
 from nnPULoss import PULoss
 from trainer import Trainer
@@ -71,7 +72,7 @@ def process_args(arguments):
                         help='Directory to output the result')
     parser.add_argument('--validation', '-v', default=False, type= str2bool,
                         help='Use of a validation dataset')
-    parser.add_argument('--Brats2020_2d', '-2dBrats', default =False, type= str2bool,
+    parser.add_argument('--Brats2020_is_2d', '-2dBrats', default =False, type= str2bool,
                         help='Determine if the converted 2D Brats2020 must be used')
 
     args = parser.parse_args(arguments)
@@ -85,9 +86,6 @@ def process_args(arguments):
     elif args.preset =="FocalLoss":
         args.loss = "FocalLoss"
 
-    # if args.Brats2020_2d == True:
-    #     temp_name = '2D_BraTS2020 RatioTrainValid ' + str(args.ratio_train_valid) +' RatioPosToNeg ' + str(args.ratio_Positive_set_to_Unlabeled)
-    #     args.rootdir = temp_name
     
     # Prior
     if args.prior is not None:
@@ -119,16 +117,17 @@ def select_loss(loss_name, prior, beta, gamma):
         loss_fn = BinaryFocalLossWithLogits(reduction='mean')
     return loss_fn
 
-def select_preprocess(Brats2020_2d, root_dir, ratio_train_valid, ratio_Positive_set_to_Unlabeled):
-    if Brats2020_2d:
-        train_data, valid_data = preprocess_brats2020_3D(root_dir, ratio_train_valid, ratio_Positive_set_to_Unlabeled)
+def select_preprocess(Brats2020_is_2d, root_dir, ratio_train_valid, ratio_Positive_set_to_Unlabeled):
+    if Brats2020_is_2d:
+        train_data, valid_data = preprocess_brats2020_2D(root_dir, ratio_train_valid, ratio_Positive_set_to_Unlabeled)
 
     else:
         train_data, valid_data = preprocess_brats2020_3D(root_dir, ratio_train_valid, ratio_Positive_set_to_Unlabeled)
+    return train_data, valid_data
 
 
 
-def select_dataloader(train_data, valid_data, dataloader_preset, batchsize, is_validation, num_worker, prior):
+def select_dataloader(Brats2020_is_2d, train_data, valid_data, dataloader_preset, batchsize, is_validation, num_worker, prior):
     kwargs = {'num_workers': num_worker, 'pin_memory': True} if torch.cuda.is_available() else {}
 
     transforms = Compose([
@@ -142,28 +141,49 @@ def select_dataloader(train_data, valid_data, dataloader_preset, batchsize, is_v
     # Normalize(mean=(0.5,), std=(0.5,)),    
     ])
 
-    if dataloader_preset == "nnPULoss":
-        train_dataset       = PU_BraTS2020_Dataset(train_data, transforms= transforms)  
-        train_dataloader    = DataLoader(train_dataset, batch_size= batch_size, shuffle= True, **kwargs)     
-        if is_validation:
-            valid_dataset       = PN_BraTS2020_Dataset(valid_data, transforms= transforms)
-            valid_dataloader    = DataLoader(valid_dataset, batch_size= batch_size, shuffle= False, **kwargs)
+    if Brats2020_is_2d:
+        if dataloader_preset == "nnPULoss":
+            train_dataset       = PU_BraTS2020_Dataset_2D(train_data, transforms= transforms)  
+            train_dataloader    = DataLoader(train_dataset, batch_size= batch_size, shuffle= True, **kwargs)     
+            if is_validation:
+                valid_dataset       = PN_BraTS2020_Dataset_2D(valid_data, transforms= transforms)
+                valid_dataloader    = DataLoader(valid_dataset, batch_size= batch_size, shuffle= False, **kwargs)
+            else:
+                valid_dataloader = None
+        elif dataloader_preset == "BCELoss" or "FocalLoss":
+            train_dataset       = BCE_BraTS2020_Dataset_2D(train_data, transforms= transforms)
+            train_dataloader    = DataLoader(train_dataset, batch_size= batch_size, shuffle= True, **kwargs)       
+            if is_validation:
+                valid_dataset       = BCE_BraTS2020_Dataset_2D(valid_data, transforms= transforms)
+                valid_dataloader    = DataLoader(valid_dataset, batch_size= batch_size, shuffle= False, **kwargs)
+            else:
+                valid_dataloader = None
         else:
-            valid_dataloader = None
-    elif dataloader_preset == "BCELoss" or "FocalLoss":
-        train_dataset       = BCE_BraTS2020_Dataset(train_data, transforms= transforms)
-        train_dataloader    = DataLoader(train_dataset, batch_size= batch_size, shuffle= True, **kwargs)       
-        if is_validation:
-            valid_dataset       = BCE_BraTS2020_Dataset(valid_data, transforms= transforms)
-            valid_dataloader    = DataLoader(valid_dataset, batch_size= batch_size, shuffle= False, **kwargs)
-        else:
-            valid_dataloader = None
+            raise ValueError('Unidentified preset has been chosen ')
     else:
-        raise ValueError('Unidentified preset has been chosen ')
+        if dataloader_preset == "nnPULoss":
+            train_dataset       = PU_BraTS2020_Dataset_3D(train_data, transforms= transforms)  
+            train_dataloader    = DataLoader(train_dataset, batch_size= batch_size, shuffle= True, **kwargs)     
+            if is_validation:
+                valid_dataset       = PN_BraTS2020_Dataset_3D(valid_data, transforms= transforms)
+                valid_dataloader    = DataLoader(valid_dataset, batch_size= batch_size, shuffle= False, **kwargs)
+            else:
+                valid_dataloader = None
+        elif dataloader_preset == "BCELoss" or "FocalLoss":
+            train_dataset       = BCE_BraTS2020_Dataset_3D(train_data, transforms= transforms)
+            train_dataloader    = DataLoader(train_dataset, batch_size= batch_size, shuffle= True, **kwargs)       
+            if is_validation:
+                valid_dataset       = BCE_BraTS2020_Dataset_2D(valid_data, transforms= transforms)
+                valid_dataloader    = DataLoader(valid_dataset, batch_size= batch_size, shuffle= False, **kwargs)
+            else:
+                valid_dataloader = None
+        else:
+            raise ValueError('Unidentified preset has been chosen ')
+
 
     if prior is None:
         print('Positive Prior automatically computed')
-        prior = train_dataset.get_prior() #if dataloader_preset == "nnPU" else None
+        prior = train_dataset.get_prior() if dataloader_preset == "nnPULoss" else None
 
     
     print('IMG status:', train_dataset[0]['img'].shape, train_dataset[0]['img'].dtype, train_dataset[0]['img'].type())
@@ -213,6 +233,7 @@ def print_info_before_training(args):
     print("gamma from nnPULoss:\t {}".format(args.gamma))
     print("validation dataset:\t {}".format(args.validation))
     print("Num of Workers:\t {}".format(args.num_worker))
+    print("Use a converted 2D BraTS2020 dataset: {}".format(args.Brats2020_is_2d))
     print("Ratio to seperate data into train and validation dataset:\t {}".format(args.ratio_train_valid))
     print("Ratio of Positive voxel set as Neative:\t {}".format(args.ratio_Positive_set_to_Unlabeled))
     print("")
@@ -222,9 +243,9 @@ def run_trainer(arguments):
     print("\nTrainer setup\n")
     args = process_args(arguments)
 
-    train_data, valid_data = select_preprocess(args.rootdir, args.ratio_train_valid, args.ratio_Positive_set_to_Unlabeled)
+    train_data, valid_data = select_preprocess(args.Brats2020_is_2d, args.rootdir, args.ratio_train_valid, args.ratio_Positive_set_to_Unlabeled)
 
-    dataloader_data = select_dataloader(train_data,valid_data, args.preset, args.batchsize, args.validation, args.num_worker, args.prior)
+    dataloader_data = select_dataloader(args.Brats2020_is_2d, train_data,valid_data, args.preset, args.batchsize, args.validation, args.num_worker, args.prior)
     train_dataloader, valid_dataloader, args.prior = dataloader_data
 
     model       = unet().to(args.device)
