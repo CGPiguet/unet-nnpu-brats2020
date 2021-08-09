4000fa00e650a6ea2f55b47f0d22566ed4ece8c8
CGPiguet <48480432+CGPiguet@users.noreply.github.com>
2021-08-09 19:04:37 +0200

    Add a code to save Prediction from validation

---
+++ run_ValidSavePred.py
@@ -0,0 +1,126 @@
+import sys
+import argparse
+import os
+
+import torch 
+import torch.nn as nn
+import numpy as np
+from utils import *
+from model import *
+
+
+
+def process_args(arguments):
+    parser = argparse.ArgumentParser(
+        description='Determine ',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    
+    
+    parser.add_argument('--rootdir', '-r', type=str, default="MICCAI_BraTS2020_TrainingData",
+                        help='Root directory of the BraTS2020')
+    parser.add_argument('--device', '-d', type=torch.device, default= torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+                        help='Determine the torch device, AutoDetection per default')
+    
+    parser.add_argument('--ratio_train_valid', '-rtv', default=0.8, type=float,
+                        help='Ratio between validation and training dataset')
+    parser.add_argument('--ratio_Positive_set_to_Unlabeled', '-rpu', default=0.95, type=float,
+                         help='Ratio of Positive class that will be set as Negative') 
+    parser.add_argument('--batchsize', '-b', type=int, default=16,
+                        help='Mini batch size')
+    parser.add_argument('--Brats2020_is_2d', '-2dBrats', default = True, type= str2bool,
+                        help='Determine if a converted 2D Brats2020 must be used, if set to true, convert automatically')
+    
+    parser.add_argument('--preset', '-p', type=str, default= 'nnPULoss',
+                        choices=['nnPULoss','BCELoss', 'FocalLoss'],
+                        help="Preset of configuration\n"
+                             "nnPULoss: With nnPU criterion\n"
+                             "BCELoss: With BinaryCrossEntropy\n"
+                             "FocalLoss: With BinaryCrossEntropy\n")
+    
+    parser.add_argument('--prior', '-pr', default= 0.5, type=float,
+                        help='Prior for nnPULoss')
+    parser.add_argument('--beta', '-B', default=0., type=float,
+                        help='Beta parameter of nnPU')
+    parser.add_argument('--gamma', '-G', default=1., type=float,
+                        help='Gamma parameter of nnPU')
+    
+    parser.add_argument('--stepsize', '-s', default=1e-4, type=float,
+                        help='Stepsize of gradient method')
+    
+    parser.add_argument('--optimizer','-opti', type=str, default="SGD", choices=['SGD', 'Adam','AdaGrad'],
+                        help='Selection of the optimizer')
+    
+    parser.add_argument('--load_model', '-load_model', default = False, type= str,
+                        help='Continue training')
+    
+    args = parser.parse_args(arguments)
+    
+    
+    args.model_folder_name, args.name = os.path.split(args.load_model)
+    args.path_to_last_model = args.load_model
+    
+
+    if args.preset == "nnPULoss":
+        args.loss = "nnPULoss"
+    elif args.preset == "BCELoss":
+        args.loss = "BCELoss"
+    elif args.preset =="FocalLoss":
+        args.loss = "FocalLoss"
+        
+    return args
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def main(arguments):
+    """ Get the dataloader"""
+    args = process_args(arguments)
+    train_data, valid_data = select_preprocess(args.Brats2020_is_2d, args.rootdir, args.ratio_train_valid, args.ratio_Positive_set_to_Unlabeled)
+    dataloader_data = select_dataloader(args.Brats2020_is_2d, train_data, valid_data, args.preset, 1, True, args.num_worker, args.prior)
+    _, valid_dataloader, args.prior = dataloader_data
+    
+    """Get model"""
+    model           = unet().to(args.device)
+    criterion       = select_loss(args.loss, args.prior, args.beta, args.gamma)
+    model.load_state_dict(torch.load(args.path_to_last_model)['model_state_dict'])
+    epoch = torch.load(args.path_to_last_model)['original_epoch']
+    
+    """Create folder to save the prediction"""        
+    folder_name = 'RatioTrainValid {} RatioPosToNeg {} Epoch {}'.format(args.ratio_train_valid, args.ratio_Positive_set_to_Unlabeled, epoch)
+    folder_path = os.path.join(args.model_folder_name,'ValidPredOnly', folder_name)
+    try:
+        os.mkdir(folder_path)
+    except:
+        pass
+  
+    loss_array = []
+    pred_array = []
+    for i, data in enumerate(valid_dataloader):
+      input, target = data['img'], data['target']
+      input, target = input.to(args.device), target.to(args.device) # Send to device (GPU or CPU)
+
+      with torch.no_grad():
+        output        = model(input)
+
+        loss, x_grad  = criterion(output.squeeze(), target.squeeze())
+        loss_value    = loss.item()
+        loss_array.append(loss_value)
+        pred_array.append(output.detach().cpu())
+        
+    save_name = os.path.join(folder_path, 'prediction.pth')    
+    torch.save(pred_array)
+
+    
+    
+    
+    
+if __name__ == '__main__':
+    main(sys.argv[1:])
